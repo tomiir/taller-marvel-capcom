8dd0f04dae4440a35b34275a2eb89de4f2bcaa0a
Ariii1996 <ariel.piro@hotmail.com>
2019-06-08 18:25:51 -0300

    nuevas animaciones

--- Animations/VenomAnimations/VenomWalkBackwardsAnimation.cpp
+++ Animations/VenomAnimations/VenomWalkBackwardsAnimation.cpp
@@ -12,16 +12,16 @@ void VenomWalkBackwardsAnimation::init() {
     animationRate = 11;
     animationLength = 10;
 
-    vector<int> frame0 = {0, 1120, 560, 238};
-    vector<int> frame1 = {560, 1120, 560, 238};
-    vector<int> frame2 = {1120, 1120, 560, 238};
-    vector<int> frame3 = {1680, 1120, 560, 238};
-    vector<int> frame4 = {2240, 1120, 560, 238};
-    vector<int> frame5 = {2800, 1120, 560, 238};
-    vector<int> frame6 = {3360, 1120, 560, 238};
-    vector<int> frame7 = {3920, 1120, 560, 238};
-    vector<int> frame8 = {4480, 1120, 560, 238};
-    vector<int> frame9 = {5040, 1120, 560, 238};
+    vector<int> frame0 = {0, 476, 560, 238};
+    vector<int> frame1 = {560, 476, 560, 238};
+    vector<int> frame2 = {1120, 476, 560, 238};
+    vector<int> frame3 = {1680, 476, 560, 238};
+    vector<int> frame4 = {2240, 476, 560, 238};
+    vector<int> frame5 = {2800, 476, 560, 238};
+    vector<int> frame6 = {3360, 476, 560, 238};
+    vector<int> frame7 = {3920, 476, 560, 238};
+    vector<int> frame8 = {4480, 476, 560, 238};
+    vector<int> frame9 = {5040, 476, 560, 238};
 
 
     animation.push_back(frame0);
--- Animations/VenomAnimations/VenomWalkFowardAnimation.cpp
+++ Animations/VenomAnimations/VenomWalkFowardAnimation.cpp
@@ -12,16 +12,16 @@ void VenomWalkFowardAnimation::init() {
     animationRate = 11;
     animationLength = 10;
 
-    vector<int> frame0 = {0, 560, 560, 238};
-    vector<int> frame1 = {560, 560, 560, 238};
-    vector<int> frame2 = {1120, 560, 560, 238};
-    vector<int> frame3 = {1680, 560, 560, 238};
-    vector<int> frame4 = {2240, 560, 560, 238};
-    vector<int> frame5 = {2800, 560, 560, 238};
-    vector<int> frame6 = {3360, 560, 560, 238};
-    vector<int> frame7 = {3920, 560, 560, 238};
-    vector<int> frame8 = {4480, 560, 560, 238};
-    vector<int> frame9 = {5040, 560, 560, 238};
+    vector<int> frame0 = {0, 238, 560, 238};
+    vector<int> frame1 = {560, 238, 560, 238};
+    vector<int> frame2 = {1120, 238, 560, 238};
+    vector<int> frame3 = {1680, 238, 560, 238};
+    vector<int> frame4 = {2240, 238, 560, 238};
+    vector<int> frame5 = {2800, 238, 560, 238};
+    vector<int> frame6 = {3360, 238, 560, 238};
+    vector<int> frame7 = {3920, 238, 560, 238};
+    vector<int> frame8 = {4480, 238, 560, 238};
+    vector<int> frame9 = {5040, 238, 560, 238};
 
 
     animation.push_back(frame0);
