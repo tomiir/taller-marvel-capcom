0e8bfa2b753140c0923475df731a070ccc0e4357
feeerr95 <feeerr95@gmail.com>
2019-06-12 21:34:33 -0300

    Agregue lo que faltaba en venom

---
+++ Animations/VenomAnimations/VenomAirKickedAnimation.h
@@ -0,0 +1,20 @@
+//
+// Created by fer on 12/06/19.
+//
+
+#ifndef TALLER_MARVEL_CAPCOM_VENOMAIRKICKEDANIMATION_H
+#define TALLER_MARVEL_CAPCOM_VENOMAIRKICKEDANIMATION_H
+
+#include "../Animation.h"
+
+
+class VenomAirKickedAnimation: public Animation {
+
+public:
+    VenomAirKickedAnimation();
+    ~VenomAirKickedAnimation() = default;
+    void init() override ;
+};
+
+
+#endif //TALLER_MARVEL_CAPCOM_VENOMAIRKICKEDANIMATION_H
---
+++ Animations/VenomAnimations/VenomStandKickedAnimation.h
@@ -0,0 +1,19 @@
+//
+// Created by fer on 12/06/19.
+//
+
+#ifndef TALLER_MARVEL_CAPCOM_VENOMSTANDKICKEDANIMATION_H
+#define TALLER_MARVEL_CAPCOM_VENOMSTANDKICKEDANIMATION_H
+
+#include "../Animation.h"
+
+
+class VenomStandKickedAnimation: public Animation {
+
+public:
+    VenomStandKickedAnimation();
+    ~VenomStandKickedAnimation() = default;
+    void init() override ;
+};
+
+#endif //TALLER_MARVEL_CAPCOM_VENOMSTANDKICKEDANIMATION_H
