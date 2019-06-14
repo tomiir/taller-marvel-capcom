f42f348f4684a8ac83bb0242970d78844195bbce
Ariii1996 <ariel.piro@hotmail.com>
2019-06-14 18:04:06 -0300

    Animaciones Venom ok

--- Animations/VenomAnimations/VenomStrongStandKickAnimation.cpp
+++ Animations/VenomAnimations/VenomStrongStandKickAnimation.cpp
@@ -8,19 +8,23 @@
 void VenomStrongStandKickAnimation::init() {
 
     startTime = SDL_GetTicks();
-    animationRate = 8;
-    animationLength = 9;
+    animationRate = 5;
+    animationLength = 13;
 
 
     vector<int> frame0 = {0, 4284, 560, 238};
     vector<int> frame1 = {560, 4284, 560, 238};
     vector<int> frame2 = {1120, 4284, 560, 238};
     vector<int> frame3 = {1680, 4284, 560, 238};
-    vector<int> frame4 = {2240, 4284, 560, 238};
-    vector<int> frame5 = {2800, 4284, 560, 238};
-    vector<int> frame6 = {3360, 4284, 560, 238};
-    vector<int> frame7 = {3920, 4284, 560, 238};
-    vector<int> frame8 = {4480, 4284, 560, 238};
+    vector<int> frame4 = {1680, 4284, 560, 238};
+    vector<int> frame5 = {1680, 4284, 560, 238};
+    vector<int> frame6 = {2240, 4284, 560, 238};
+    vector<int> frame7 = {2240, 4284, 560, 238};
+    vector<int> frame8 = {2240, 4284, 560, 238};
+    vector<int> frame9 = {2800, 4284, 560, 238};
+    vector<int> frame10 = {3360, 4284, 560, 238};
+    vector<int> frame11 = {3920, 4284, 560, 238};
+    vector<int> frame12 = {4480, 4284, 560, 238};
 
 
     animation.push_back(frame0);
@@ -32,6 +36,10 @@ void VenomStrongStandKickAnimation::init() {
     animation.push_back(frame6);
     animation.push_back(frame7);
     animation.push_back(frame8);
+    animation.push_back(frame9);
+    animation.push_back(frame10);
+    animation.push_back(frame11);
+    animation.push_back(frame12);
 
 
 }
