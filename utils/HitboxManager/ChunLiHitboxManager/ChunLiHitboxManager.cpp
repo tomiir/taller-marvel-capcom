d8ee3c39cbf4b230a35f35c56412132d77b3428f
feeerr95 <feeerr95@gmail.com>
2019-06-15 19:17:48 -0300

    hitboxes de chun li agregadas

--- utils/HitboxManager/ChunLiHitboxManager/ChunLiHitboxManager.cpp
+++ utils/HitboxManager/ChunLiHitboxManager/ChunLiHitboxManager.cpp
@@ -8,47 +8,47 @@ ChunLiHitboxManager::ChunLiHitboxManager() :  HitboxManager(){
 
     currentState = "still";
 
-    SDL_Rect hitboxWalkBackward = SDL_Rect{503, 345, 168, 198};
-    SDL_Rect hitboxWalkFoward = SDL_Rect{503, 345,  168, 198};
-    SDL_Rect hitboxStill = SDL_Rect{503, 375, 178, 153};
-    SDL_Rect hitboxCrowchedDown = SDL_Rect{525, 425, 150, 103};
-    SDL_Rect hitboxJump = SDL_Rect{480, 275, 190, 158};
+    SDL_Rect hitboxWalkBackward = SDL_Rect{460, 238, 123, 208};
+    SDL_Rect hitboxWalkFoward = SDL_Rect{485, 248, 123, 200};
+    SDL_Rect hitboxStill = SDL_Rect{460, 238, 143, 210};
+    SDL_Rect hitboxCrowchedDown = SDL_Rect{470, 283, 143, 165};
+    SDL_Rect hitboxJump = SDL_Rect{470, 215, 143, 183};
 
-    SDL_Rect hitboxStandKicked = SDL_Rect{468, 395, 148, 133};
-    SDL_Rect hitboxDownKicked = SDL_Rect{488, 375, 145, 175};
-    SDL_Rect hitboxAirKicked = SDL_Rect{468, 313, 148, 128};
+    SDL_Rect hitboxStandKicked = SDL_Rect{450, 245, 138, 205};
+    SDL_Rect hitboxDownKicked = SDL_Rect{470, 275, 138, 168};
+    SDL_Rect hitboxAirKicked = SDL_Rect{460, 213, 105, 168};
 
-    SDL_Rect hitboxStandGuard = SDL_Rect{445, 290, 220, 238};
-    SDL_Rect hitboxDownGuard = SDL_Rect{473, 390, 220, 155};
-    SDL_Rect hitboxAirGuard = SDL_Rect{498, 208, 195, 225};
+    SDL_Rect hitboxStandGuard = SDL_Rect{475, 228, 115, 213};
+    SDL_Rect hitboxDownGuard = SDL_Rect{475, 275, 140, 165};
+    SDL_Rect hitboxAirGuard = SDL_Rect{460, 205, 120, 165};
 
-    SDL_Rect hitboxWeakAirPunchBody = SDL_Rect{515, 255, 133, 143};
-    SDL_Rect hitboxWeakAirPunchArm = SDL_Rect{615, 398, 80, 70};
-    SDL_Rect hitboxWeakStandPunchBody = SDL_Rect{540, 358, 138, 168};
-    SDL_Rect hitboxWeakStandPunchArm = SDL_Rect{678, 368, 133, 40};
-    SDL_Rect hitboxWeakDownPunchBody = SDL_Rect{548, 430, 160, 113};
-    SDL_Rect hitboxWeakDownPunchArm = SDL_Rect{695, 485, 143, 40};
+    SDL_Rect hitboxWeakAirPunchBody = SDL_Rect{493, 193, 120, 165};
+    SDL_Rect hitboxWeakAirPunchArm = SDL_Rect{613, 255, 83, 50};
+    SDL_Rect hitboxWeakStandPunchBody = SDL_Rect{493, 235, 120, 213};
+    SDL_Rect hitboxWeakStandPunchArm = SDL_Rect{613, 255, 105, 33};
+    SDL_Rect hitboxWeakDownPunchBody = SDL_Rect{493, 288, 120, 160};
+    SDL_Rect hitboxWeakDownPunchArm = SDL_Rect{610, 315, 105, 33};
 
-    SDL_Rect hitboxStrongAirPunchBody = SDL_Rect{530, 270, 125, 190};
-    SDL_Rect hitboxStrongAirPunchArm = SDL_Rect{655, 270, 133, 158};
-    SDL_Rect hitboxStrongStandPunchBody = SDL_Rect{575, 285, 125, 190};
-    SDL_Rect hitboxStrongStandPunchArm = SDL_Rect{700, 300, 180, 158};
-    SDL_Rect hitboxStrongDownPunchBody = SDL_Rect{583, 428, 188, 113};
-    SDL_Rect hitboxStrongDownPunchArm = SDL_Rect{738, 335, 138, 205};
+    SDL_Rect hitboxStrongAirPunchBody = SDL_Rect{480, 188, 105, 160};
+    SDL_Rect hitboxStrongAirPunchArm = SDL_Rect{585, 235, 55, 113};
+    SDL_Rect hitboxStrongStandPunchBody = SDL_Rect{558, 263, 93, 188};
+    SDL_Rect hitboxStrongStandPunchArm = SDL_Rect{650, 253, 98, 63};
+    SDL_Rect hitboxStrongDownPunchBody = SDL_Rect{558, 285, 128, 155};
+    SDL_Rect hitboxStrongDownPunchArm = SDL_Rect{685, 310, 118, 38};
 
-    SDL_Rect hitboxWeakAirKickBody = SDL_Rect{498, 263, 150, 108};
-    SDL_Rect hitboxWeakAirKickLeg = SDL_Rect{648, 320, 148, 103};
-    SDL_Rect hitboxWeakStandKickBody = SDL_Rect{498, 335, 150, 208};
-    SDL_Rect hitboxWeakStandKickLeg = SDL_Rect{648, 350, 193, 55};
-    SDL_Rect hitboxWeakDownKickBody = SDL_Rect{538, 420, 110, 123};
-    SDL_Rect hitboxWeakDownKickLeg = SDL_Rect{648, 455, 200, 55};
+    SDL_Rect hitboxWeakAirKickBody = SDL_Rect{445, 218, 128, 130};
+    SDL_Rect hitboxWeakAirKickLeg = SDL_Rect{573, 283, 153, 65};
+    SDL_Rect hitboxWeakStandKickBody = SDL_Rect{488, 230, 85, 225};
+    SDL_Rect hitboxWeakStandKickLeg = SDL_Rect{573, 230, 160, 83};
+    SDL_Rect hitboxWeakDownKickBody = SDL_Rect{488, 338, 160, 60};
+    SDL_Rect hitboxWeakDownKickLeg = SDL_Rect{603, 398, 160, 60};
 
-    SDL_Rect hitboxStrongAirKickBody = SDL_Rect{488, 240, 155, 155};
-    SDL_Rect hitboxStrongAirKickLeg = SDL_Rect{643, 168, 123, 228};
-    SDL_Rect hitboxStrongStandKickBody = SDL_Rect{510, 258, 150, 243};
-    SDL_Rect hitboxStrongStandKickLeg = SDL_Rect{660, 258, 188, 200};
-    SDL_Rect hitboxStrongDownKickBody = SDL_Rect{615, 383, 150, 155};
-    SDL_Rect hitboxStrongDownKickLeg = SDL_Rect{765, 460, 260, 78};
+    SDL_Rect hitboxStrongAirKickBody = SDL_Rect{473, 198, 130, 158};
+    SDL_Rect hitboxStrongAirKickLeg = SDL_Rect{603, 220, 150, 70};
+    SDL_Rect hitboxStrongStandKickBody = SDL_Rect{428, 300, 153, 148};
+    SDL_Rect hitboxStrongStandKickLeg = SDL_Rect{505, 170, 108, 130};
+    SDL_Rect hitboxStrongDownKickBody = SDL_Rect{460, 300, 153, 148};
+    SDL_Rect hitboxStrongDownKickLeg = SDL_Rect{613, 293, 210, 60};
 
 
     stateHitBoxes["still"] = { hitboxStill };
