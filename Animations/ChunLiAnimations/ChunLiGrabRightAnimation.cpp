a2bc7fc0e0f1b783bfa138c6715528c0172a5336
Ariii1996 <ariel.piro@hotmail.com>
2019-06-25 19:31:06 -0300

    ChunLi grab ok

--- Animations/ChunLiAnimations/ChunLiGrabRightAnimation.cpp
+++ Animations/ChunLiAnimations/ChunLiGrabRightAnimation.cpp
@@ -8,13 +8,19 @@
 void ChunLiGrabRightAnimation::init() {
 
     startTime = SDL_GetTicks();
-    animationRate = 10;
-    animationLength = 4;
+    animationRate = 7;
+    animationLength = 14;
 
     vector<int> frame0 = {0, 4410, 407, 210};
     vector<int> frame1 = {407, 4410, 407, 210};
     vector<int> frame2 = {814, 4410, 407, 210};
     vector<int> frame3 = {1221, 4410, 407, 210};
+
+    vector<int> frame4 = {0, 0, 407, 210};
+    vector<int> frame5 = {407, 0, 407, 210};
+    vector<int> frame6 = {814, 0, 407, 210};
+    vector<int> frame7 = {1221, 0, 407, 210};
+    vector<int> frame8 = {1628, 0, 407, 210};
 
 
 
@@ -22,6 +28,16 @@ void ChunLiGrabRightAnimation::init() {
     animation.push_back(frame1);
     animation.push_back(frame2);
     animation.push_back(frame3);
+    animation.push_back(frame3);
+    animation.push_back(frame4);
+    animation.push_back(frame5);
+    animation.push_back(frame5);
+    animation.push_back(frame6);
+    animation.push_back(frame6);
+    animation.push_back(frame7);
+    animation.push_back(frame7);
+    animation.push_back(frame8);
+    animation.push_back(frame8);
 }
 
 ChunLiGrabRightAnimation::ChunLiGrabRightAnimation() : Animation() {
